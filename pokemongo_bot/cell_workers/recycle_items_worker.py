451a66f52dad48bede80238be673dbdf708d1754
Julian Meyer <julianmeyer2000@gmail.com>
2016-07-26 13:53:03 -0700

    Added RecycleItemsWorker and replaced some code in SeenFortWorker

---
+++ pokemongo_bot/cell_workers/recycle_items_worker.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+
+import time
+
+from pgoapi.utilities import f2i
+from pokemongo_bot import logger
+from pokemongo_bot.human_behaviour import sleep
+from pokemongo_bot.cell_workers.utils import format_time
+
+
+class RecycleItemsWorker(object):
+
+    def __init__(self, bot):
+        self.api = bot.api
+        self.bot = bot
+        self.position = bot.position
+        self.config = bot.config
+        self.item_list = bot.item_list
+
+    def work(self):
+        self.bot.update_inventory()
+        if self.config.recycle_items:
+            for item in self.bot.inventory:
+                if str(item["item_id"]) in self.config.item_filter:
+                    amount_to_keep = self.config.item_filter.get(str(item["item_id"])).get("keep")
+                    if amount_to_keep is None:
+                        continue
+                    amount_to_drop = item["count"] - amount_to_keep
+                    if amount_to_drop <= 0:
+                        continue
+                    logger.log("[+] Recycling: {} x {}...".format(self.item_list[str(item["item_id"])], amount_to_drop), 'green')
+                    self.bot.drop_item(item["item_id"], amount_to_drop)
