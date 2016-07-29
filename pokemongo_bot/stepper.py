30a52f50b8e0ffc12f3aae405e5c123b1df099da
Eric Ahn <wchill1337@gmail.com>
2016-07-28 22:15:30 -0700

    Use API wrapper

--- pokemongo_bot/stepper.py
+++ pokemongo_bot/stepper.py
@@ -2,13 +2,20 @@
 
 from datetime import datetime
 from math import ceil
+from typing import Optional, List
 import json
+<<<<<<< 05f8f132274ab4466d0b205458bc992e653424e3
 import googlemaps
 from s2sphere import CellId, LatLng
 from pgoapi.utilities import f2i
+=======
+
+from s2sphere import CellId, LatLng # type: ignore
+>>>>>>> Refactor step 3
 from pokemongo_bot.human_behaviour import sleep, random_lat_long_delta
-from pokemongo_bot.utils import distance, i2f, format_time, convert_to_utf8
+from pokemongo_bot.utils import distance, format_time, f2i
 import pokemongo_bot.logger as logger
+
 
 class Stepper(object):
 
@@ -16,7 +23,7 @@ class Stepper(object):
 
     def __init__(self, bot):
         self.bot = bot
-        self.api = bot.api
+        self.api_wrapper = bot.api_wrapper
         self.config = bot.config
 
         self.pos = 1
@@ -27,19 +34,19 @@ class Stepper(object):
         self.origin_lon = self.bot.position[1]
 
     def take_step(self):
-        position = (0, 0, 0)
         if self.first_step:
             self.first_step = False
             position = (self.origin_lat, self.origin_lon, 0.0)
         else:
-            position_lat, position_lng, _ = self.api.get_position()
-            position = (i2f(position_lat), i2f(position_lng), 0.0)
+            position_lat, position_lng, _ = self.api_wrapper.get_position()
+            position = (position_lat, position_lng, 0.0)
 
-        self.api.set_position(*position)
+        self.api_wrapper.set_position(*position)
         self._work_at_position(position[0], position[1], True)
         sleep(5)
 
     def walk_to(self, speed, lat, lng, alt):
+<<<<<<< 05f8f132274ab4466d0b205458bc992e653424e3
         position_lat, position_lng, _ = self.api.get_position()
 
         # ask google for directions
@@ -49,7 +56,7 @@ class Stepper(object):
             gmaps = googlemaps.Client(key=self.config.gmapkey)
 
             now = datetime.now()
-            start = "{},{}".format(i2f(position_lat), i2f(position_lng))
+            start = "{},{}".format(position_lat, position_lng)
             end = "{},{}".format(lat, lng)
             directions_result = gmaps.directions(start, end, mode="walking", departure_time=now)
 
@@ -67,16 +74,21 @@ class Stepper(object):
                         )
             else:
                 # If google doesn't know the way, then we just have to go as the crow flies
-                self._do_walk_to(speed, i2f(position_lat), i2f(position_lng), lat, lng, alt, 25)
+                self._do_walk_to(speed, position_lat, position_lng, lat, lng, alt, 25)
 
         else:
-            self._do_walk_to(speed, i2f(position_lat), i2f(position_lng), lat, lng, alt, 25)
+            self._do_walk_to(speed, position_lat, position_lng, lat, lng, alt, 25)
 
         logger.log("[#] Walking Finished")
 
     def _do_walk_to(self, speed, from_lat, from_lng, to_lat, to_lng, alt, delta_factor):
+        # type: (float, float, float, float, float, float, float) -> None
 
         dist = distance(from_lat, from_lng, to_lat, to_lng)
+=======
+        position_lat, position_lng, _ = self.api_wrapper.get_position()
+        dist = distance(position_lat, position_lng, lat, lng)
+>>>>>>> Refactor step 3
         steps = (dist / (self.AVERAGE_STRIDE_LENGTH_IN_METRES * speed))
 
         logger.log("[#] Walking from " + str((from_lat, from_lng)) + " to " + str(str((to_lat, to_lng))) + " for approx. " + str(format_time(ceil(steps))))
@@ -85,22 +97,30 @@ class Stepper(object):
             d_long = (to_lng - from_lng) / steps
 
             for _ in range(int(steps)):
+<<<<<<< 05f8f132274ab4466d0b205458bc992e653424e3
                 position_lat, position_lng, _ = self.api.get_position()
-                c_lat = i2f(position_lat) + d_lat + random_lat_long_delta(delta_factor)
-                c_long = i2f(position_lng) + d_long + random_lat_long_delta(delta_factor)
+                c_lat = position_lat + d_lat + random_lat_long_delta(delta_factor)
+                c_long = position_lng + d_long + random_lat_long_delta(delta_factor)
                 self.api.set_position(c_lat, c_long, alt)
+=======
+                position_lat, position_lng, _ = self.api_wrapper.get_position()
+                c_lat = position_lat + d_lat + random_lat_long_delta()
+                c_long = position_lng + d_long + random_lat_long_delta()
+                self.api_wrapper.set_position(c_lat, c_long, alt)
+>>>>>>> Refactor step 3
 
                 self.bot.heartbeat()
                 sleep(1)  # sleep one second plus a random delta
 
-                position_lat, position_lng, _ = self.api.get_position()
-                self._work_at_position(i2f(position_lat), i2f(position_lng), False)
+                position_lat, position_lng, _ = self.api_wrapper.get_position()
+                self._work_at_position(position_lat, position_lng, False)
 
             self.bot.heartbeat()
 
     def _get_cell_id_from_latlong(self, radius=10):
-        position_lat, position_lng, _ = self.api.get_position()
-        origin = CellId.from_lat_lng(LatLng.from_degrees(i2f(position_lat), i2f(position_lng))).parent(15)
+        # type: (Optional[float]) -> List[str]
+        position_lat, position_lng, _ = self.api_wrapper.get_position()
+        origin = CellId.from_lat_lng(LatLng.from_degrees(position_lat, position_lng)).parent(15)
         walk = [origin.id()]
 
         # 10 before and 10 after
@@ -114,28 +134,29 @@ class Stepper(object):
         return sorted(walk)
 
     def _work_at_position(self, lat, lng, pokemon_only=False):
+        # type: (float, float, Optional[bool]) -> None
         cell_id = self._get_cell_id_from_latlong()
         timestamp = [0, ] * len(cell_id)
-        self.api.get_map_objects(latitude=f2i(lat),
+        self.api_wrapper.get_map_objects(latitude=f2i(lat),
                                  longitude=f2i(lng),
                                  since_timestamp_ms=timestamp,
                                  cell_id=cell_id)
 
-        response_dict = self.api.call()
+        response_dict = self.api_wrapper.call()
         if response_dict is None:
             return
         # Passing data through last-location and location
-        map_objects = response_dict.get("responses", {}).get("GET_MAP_OBJECTS")
-        if map_objects is not None:
-            with open("web/location-{}.json".format(self.config.username), "w") as outfile:
-                json.dump({"lat": lat, "lng": lng, "cells": convert_to_utf8(map_objects.get("map_cells"))}, outfile)
-            with open("data/last-location-{}.json".format(self.config.username), "w") as outfile:
-                outfile.truncate()
-                json.dump({"lat": lat, "lng": lng}, outfile)
-            if "status" in map_objects:
-                if map_objects.get("status") is 1:
-                    map_cells = map_objects.get("map_cells")
-                # Sort all by distance from current pos - eventually this should build graph and A* it
-                map_cells.sort(key=lambda x: distance(lat, lng, x["forts"][0]["latitude"], x["forts"][0]["longitude"]) if "forts" in x and x["forts"] != [] else 1e6)
-                for cell in map_cells:
-                    self.bot.work_on_cell(cell, pokemon_only)
+        map_objects = response_dict["worldmap"]
+        """
+        with open("web/location-{}.json".format(self.config.username), "w") as outfile:
+            json.dump({"lat": lat, "lng": lng, "cells": convert_to_utf8(map_objects.get("map_cells"))}, outfile)
+        """
+        with open("data/last-location-{}.json".format(self.config.username), "w") as outfile:
+            outfile.truncate()
+            json.dump({"lat": lat, "lng": lng}, outfile)
+
+        map_cells = map_objects.cells
+        # Sort all by distance from current pos - eventually this should build graph and A* it
+        map_cells.sort(key=lambda x: distance(lat, lng, x.pokestops[0].latitude, x.pokestops[0].longitude) if len(x.pokestops) > 0 else 1e6)
+        for cell in map_cells:
+            self.bot.work_on_cell(cell, pokemon_only)
