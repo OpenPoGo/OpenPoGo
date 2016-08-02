9e0b40fa8945e6606500902230dca3e1c261eb8e
Reaver01 <reaver_01@gmx.com>
2016-08-02 08:49:41 +0200

    Update fort_navigator.py
    for all cygwin

--- pokemongo_bot/navigation/fort_navigator.py
+++ pokemongo_bot/navigation/fort_navigator.py
@@ -35,7 +35,7 @@ class FortNavigator(Navigator):
                 response_dict = self.api_wrapper.call()
                 if response_dict is None:
                     fort_name = fort_id
-                elif platform.system() == "Windows" or platform.system() == "FreeBSD" or platform.system() == "CYGWIN_NT-6.1":
+                elif platform.system() == "Windows" or platform.system() == "FreeBSD" or platform.system()[:6] == "CYGWIN":
                     fort_name = repr(response_dict["fort"].fort_name)
                 else:
                     fort_name = response_dict["fort"].fort_name
