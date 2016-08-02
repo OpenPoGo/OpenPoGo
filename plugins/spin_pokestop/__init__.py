40e1cf532c3428260c566275d6264e3468645697
Reaver01 <reaver_01@gmx.com>
2016-08-02 20:08:52 +0200

    Update __init__.py

--- plugins/spin_pokestop/__init__.py
+++ plugins/spin_pokestop/__init__.py
@@ -2,7 +2,6 @@
 
 from __future__ import print_function
 import time
-import platform
 from math import ceil
 
 from pokemongo_bot.human_behaviour import sleep
@@ -69,11 +68,7 @@ def spin_pokestop(bot, pokestop=None):
                                                 latitude=pokestop.latitude,
                                                 longitude=pokestop.longitude).call()
     dist = distance(bot.stepper.current_lat, bot.stepper.current_lng, pokestop.latitude, pokestop.longitude)
-    if platform.system() == "Windows" or platform.system() == "FreeBSD" or platform.system()[:6] == "CYGWIN":
-        fort_name = repr(fort_details["fort"].fort_name)
-    else:
-        fort_name = fort_details["fort"].fort_name
-    log("Nearby PokeStop found \"{}\" ({} away)".format(fort_name,
+    log("Nearby PokeStop found \"{}\" ({} away)".format(fort_details["fort"].fort_name,
                                                         format_dist(dist, bot.config.distance_unit)), color="yellow")
 
     log("Spinning...", color="yellow")
