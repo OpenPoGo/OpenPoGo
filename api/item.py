e3dfaa880aaf4e2a3bdfaaa93626942eca066e37
Nicolas Schmitt <nicolas.schmitt@outlook.fr>
2016-08-08 22:56:37 +0200

    new socket plugin

--- api/item.py
+++ api/item.py
@@ -10,3 +10,16 @@ class Incubator(JSONEncodable):
         self.pokemon_id = data.get("pokemon_id", 0)
         self.start_km_walked = data.get("start_km_walked", 0.0)
         self.target_km_walked = data.get("target_km_walked", 0.0)
+
+    # below function are for jsonpickle,
+    # because long field are not compatible with JSON and get rounded by browser
+    def __getstate__(self):
+        state = self.__dict__.copy()
+        state["unique_id"] = str(self.unique_id)
+        state["pokemon_id"] = str(self.pokemon_id)
+        return state
+
+    def __setstate__(self, state):
+        state["unique_id"] = long(state["unique_id"])
+        state["pokemon_id"] = long(state["pokemon_id"])
+        self.__dict__.update(state)
