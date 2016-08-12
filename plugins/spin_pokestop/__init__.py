92f72ebe2f26d834980ec311ff7021446be0f266
Philipp Heuer <philipp.heuer@outlook.com>
2016-08-12 19:02:47 +0200

    Plugin: Spin pokestop - Fixed log messages (Even if the item bag is full, xp are still awarded.)

--- plugins/spin_pokestop/__init__.py
+++ plugins/spin_pokestop/__init__.py
@@ -134,8 +134,20 @@ def spin_pokestop(bot, pokestop=None):
             cooldown_time = str(format_time((pokestop_cooldown / 1000) - seconds_since_epoch))
             log("PokeStop is already on cooldown for {}.".format(cooldown_time), "red")
     elif spin_result == 4:
-        log("Item bag is full.", "red")
+        log("Loot: ", "green")
+
+        manager.fire_with_context('pokestop_visited', bot, pokestop=pokestop)
+
+        experience_awarded = spin_details.get("experience_awarded", False)
+        if experience_awarded:
+            log("+ {} XP".format(experience_awarded), "green")
+
+        log("+ Item bag is full.", "red")
+
         bot.fire("item_bag_full")
+
+        if not experience_awarded and not pokestop_cooldown:
+            log("Might be softbanned, try again later.", "red")
     else:
         log("I don't know what happened! Maybe servers are down?", "red")
 
