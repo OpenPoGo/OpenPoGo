c9e07154eebd9cb52ee9d8a0ca9b6dcdeb74df0f
Eric Ahn <wchill1337@gmail.com>
2016-08-13 20:36:56 -0700

    Move exception class

--- api/exceptions/__init__.py
+++ api/exceptions/__init__.py
@@ -1,1 +1,6 @@
-from AccountBannedException import AccountBannedException
+class AccountBannedException(Exception):
+
+    def __init__(self, message=None):
+        if message is None:
+            message = "[API] Status code 3 received. This may mean that your account is permanently banned. See https://www.reddit.com/r/pokemongodev/comments/4xkqmq/new_ban_types_and_their_causes/ for details."
+        super(AccountBannedException, self).__init__(message)
